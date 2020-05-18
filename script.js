aa35946d084723d6b653214e5cbea7f66ccbaad8
Daniel Barber <Daniel@Daniels-MacBook-Pro.local>
2020-05-18 15:42:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// var i = 0;
+// var images = [];
+// var time = 8000;
+
+// //image list
+
+// images[0] = "images/PHG.jpg";
+// images[1] = "images/PHG2.jpg";
+// images[2] = "images/PHG3.jpg";
+
+// //change images
+
+// function changeImg() {
+//   document.slide.src = images[i];
+
+//   if (i < images.length - 1) {
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+
+//   setTimeout("changeImg()", time);
+// }
+
+// window.onload = changeImg;
